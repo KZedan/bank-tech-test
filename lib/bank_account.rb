803d832f0d40fb688e187c2de3b08005636b48c9
Kareem <kareem335@yahoo.co.uk>
2019-08-27 12:19:23 +0100

    bank account allows withdrawals and shows remaining balance and date of withdrawal

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,13 @@
 class BankAccount
+  def initialize
+    @balance = 0
+  end
+
   def deposit(amount, date = Time.now)
-    {amount: amount, date: date}
+    {amount: @balance += amount, date: date}
+  end
+
+  def withdraw(amount, date = Time.now)
+    {balance: @balance -= amount, date: date}
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,4 +8,12 @@ describe BankAccount do
     date = "15/07/1993"
     expect(bankaccount.deposit(amount, date)).to eq ({amount: 1000, date: "15/07/1993"})
   end
+
+  it "allows withdrawals with date" do
+    bankaccount.deposit(1000)
+    amount = 500
+    date = "15/07/1993"
+    expect(bankaccount.withdraw(amount, date)).to eq ({balance: 500, date: "15/07/1993"})
+  end
+
 end
